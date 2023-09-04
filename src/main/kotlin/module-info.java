e576beb7a4a2f985cff0b8da509c9440dc77c3f7
Elias Perras <elias.perras@d-fine.de>
2023-09-04 22:11:16 +0200

    Initial commit

---
+++ src/main/kotlin/module-info.java
@@ -0,0 +1,12 @@
+module com.example.threebody_kt_graphics {
+    requires javafx.controls;
+    requires javafx.fxml;
+                requires kotlin.stdlib;
+    
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+                    requires org.kordamp.bootstrapfx.core;
+            
+    opens com.example.threebody_kt_graphics to javafx.fxml;
+    exports com.example.threebody_kt_graphics;
+}
